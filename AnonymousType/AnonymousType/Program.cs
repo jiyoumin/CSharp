453750addbc5a6bd564ce00b929549504b8b6c7a
jiyoumin <63236411+jiyoumin@users.noreply.github.com>
2020-08-22 21:15:57 +0900

    무명 형식
    했어요

---
+++ AnonymousType/AnonymousType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnonymousType
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "지유민", Age = 123 };
+            Console.WriteLine($"Name:{a.Name}, Age:{a.Age}");
+
+            var b = new { Subject = "C#", Scores = new int[] { 90, 80, 70, 60 } };
+            Console.Write($"Subject:{b.Subject}, Scores:");
+            foreach (var score in b.Scores)
+            {
+                Console.Write($"{score} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
