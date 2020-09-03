86e67fc3fdd83dbaa940f4fce87380c372b05e94
jiyoumin <63236411+jiyoumin@users.noreply.github.com>
2020-09-03 15:01:55 +0900

    예외 던지기(throw)
    g

---
+++ Throw/Throw/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Throw
+{
+    class Program
+    {
+        static void DoSomething(int arg)
+        {
+            if (arg < 10)
+                Console.WriteLine($"arg : {arg}");
+            else
+                throw new Exception("arg가 10보다 큽니다.");
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 1; i < 10; i += 2)
+                {
+                    DoSomething(i);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
