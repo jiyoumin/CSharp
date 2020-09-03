2868b3d80534fac608c37144dbb3f0bf61267a5e
jiyoumin <63236411+jiyoumin@users.noreply.github.com>
2020-09-03 11:31:00 +0900

    System.Array
    h

---
+++ MoreOnArray/MoreOnArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MoreOnArray
+{
+    class Program
+    {
+        private static bool CheckPassed(int score)
+        {
+            if (score >= 60)
+                return true;
+            else
+                return false;
+        }
+        private static void Print(int value)
+        {
+            Console.Write($"{value} ");
+        }
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 80, 74, 81, 90, 34 };
+            foreach (int score in scores)
+                Console.WriteLine($"{score}");
+            Console.WriteLine();
+
+            Array.Sort(scores);
+            Array.ForEach<int>(scores, new Action<int>(Print));
+            Console.WriteLine();
+
+            Console.WriteLine($"Number of dimensions : {scores.Rank}");
+            Console.WriteLine($"New length of scores : {scores.Length}");
+            Array.Resize<int>(ref scores, 10);
+            Console.WriteLine($"New length of scores : {scores.Length}");
+
+            Array.Clear(scores, 3, 7);
+            Array.ForEach<int>(scores, new Action<int>(Print));
+        }
+    }
+}
