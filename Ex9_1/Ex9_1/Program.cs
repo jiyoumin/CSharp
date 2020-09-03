230440689a601d7aeae29532b66f413fa2b180b0
jiyoumin <63236411+jiyoumin@users.noreply.github.com>
2020-09-03 10:28:55 +0900

    연습문제(1,2)
    ㅎ

---
+++ Ex9_1/Ex9_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Ex9_1
+{
+    class NameCard
+    {
+        private int age;
+        private string name;
+        public int Age
+        { 
+            get { return age; }
+            set { age = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard MyCard = new NameCard();
+
+            MyCard.Age=(19);
+            MyCard.Name=("유민");
+
+            Console.WriteLine("나이 : {0}", MyCard.Age);
+            Console.WriteLine("이름 : {0}", MyCard.Name);
+        }
+    }
+}
---
+++ Ex9_2/Ex9_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameCard = new { Name = "유민", Age = "19" };
+            Console.WriteLine("이름:{0}, 나이:{1}", nameCard.Name, nameCard.Age);
+
+            var complex = new { Real = "1", Imaginary = "2" };
+            Console.WriteLine("Real:{0},Imaginary:{1}", complex.Real, complex.Imaginary);
+        }
+    }
+}
