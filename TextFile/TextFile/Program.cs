5a5a77ab4a14122bb6509bc36e33fec40215aec4
jiyoumin <63236411+jiyoumin@users.noreply.github.com>
2020-09-04 00:56:06 +0900

    텍스트 파일 처리
    g

---
+++ TextFile/TextFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw =
+                new StreamWriter(
+                    new FileStream("a.txt", FileMode.Create));
+
+            sw.WriteLine(int.MaxValue);
+            sw.WriteLine("Good morning!");
+            sw.WriteLine(uint.MaxValue);
+            sw.WriteLine("안녕하세요!");
+            sw.WriteLine(double.MaxValue);
+
+            sw.Close();
+
+            StreamReader sr =
+                new StreamReader(
+                    new FileStream("a.txt", FileMode.Open));
+
+            Console.WriteLine($"File size : {sr.BaseStream.Length} btyes");
+
+            while (sr.EndOfStream == false)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+        }
+    }
+}
